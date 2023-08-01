d46989ae0090468d6303dd939fc5f33348a33e23
perry1224 <perry.w1224@gmail.com>
2023-07-31 23:53:16 -0400

    updated gallery

--- index.js
+++ index.js
@@ -60,3 +60,25 @@ if (typeof AOS !== 'undefined') {
   // If AOS is not defined yet, you can either wait for it to load or handle the situation accordingly
   console.warn('AOS library is not loaded yet. Make sure you have included the AOS library.');
 }
+
+// scroll to top functionality
+const scrollUpIcon = document.querySelector(".scroll-up");
+let isScrolling = false;
+
+scrollUpIcon.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (!isScrolling && window.scrollY > 0) {
+    isScrolling = true;
+    scrollUpIcon.classList.add("show");
+  } else if (isScrolling && window.scrollY === 0) {
+    isScrolling = false;
+    scrollUpIcon.classList.remove("show");
+  }
+});
